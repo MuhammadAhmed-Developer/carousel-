5e956fc99ccb827913b52b5cb7258ff07b0f2354
Muhammad Ahmed <119342876+MuhammadAhmed-Developer@users.noreply.github.com>
2023-04-20 16:22:49 +0100

    add js

--- script.js
+++ script.js
@@ -28,3 +28,25 @@
 //   progressContent.textContent = `${Math.ceil(time / 1000)}s`;
 // });
 // git a commit
+// git a commit
+// const progressCircle = document.querySelector(".autoplay-progress svg");
+// const progressContent = document.querySelector(".autoplay-progress span");
+
+// const swiperEl = document.querySelector("swiper-container");
+// swiperEl.addEventListener("autoplaytimeleft", (e) => {
+//   const [swiper, time, progress] = e.detail;
+//   progressCircle.style.setProperty("--progress", 1 - progress);
+//   progressContent.textContent = `${Math.ceil(time / 1000)}s`;
+// });
+// git a commit
+// git a commit
+// const progressCircle = document.querySelector(".autoplay-progress svg");
+// const progressContent = document.querySelector(".autoplay-progress span");
+
+// const swiperEl = document.querySelector("swiper-container");
+// swiperEl.addEventListener("autoplaytimeleft", (e) => {
+//   const [swiper, time, progress] = e.detail;
+//   progressCircle.style.setProperty("--progress", 1 - progress);
+//   progressContent.textContent = `${Math.ceil(time / 1000)}s`;
+// });
+// git a commit
